f2b199bbece1682a142edf3ebcbbe0391548f0b8
JosephSultan <105505623+JosephSultan@users.noreply.github.com>
2022-07-02 19:32:44 +0300

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Joseph Sultan';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'It's just a proof of concept, nothing much, ehh';
+    : 'Itis just a proof of concept, nothing much ehh';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All approved.';
